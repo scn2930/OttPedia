f2132891f2fdd96c5472e75c937d7181adcbf747
FED <105217969+scn2930@users.noreply.github.com>
2024-10-04 23:05:10 +0900

    카테고리 수정

--- src/features/CategoryPage/style.ts
+++ src/features/CategoryPage/style.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 export const CategoryStyle = styled.div`
-  padding-top: 100px;
+  padding-top: 80px;
   overflow: hidden;
 
   .tagsWrap {
+    padding-top: 20px;
     display: flex;
     flex-direction: row;
     gap: 10px;
@@ -14,9 +15,16 @@ export const CategoryStyle = styled.div`
   }
   .tags {
     width: 100%;
-    padding: 20px 35px;
-    border-radius: 60%;
+    padding: 17px 37px;
+    border: 1px solid #444;
+    border-radius: 30px;
     white-space: nowrap;
-    background-color: gray;
+    background-color: #262626;
+    color: #d9d9d9;
+    transition: transform 0.4s ease-in-out;
+    &:hover {
+      cursor: pointer;
+      transform: translate(0, -10px);
+    }
   }
 `;
